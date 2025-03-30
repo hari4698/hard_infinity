efa85b8588d76c65023e8aa2e01952d6d38ed626
Hariharan Sreenivas <hari@Hariharans-MacBook-Pro.local>
2025-03-29 20:32:22 -0700

    Added Shadcn and tailwind

--- web/app.config.ts
+++ web/app.config.ts
@@ -1,6 +1,7 @@
 // app.config.ts
 import { defineConfig } from '@tanstack/react-start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   vite: {
@@ -8,6 +9,7 @@ export default defineConfig({
       tsConfigPaths({
         projects: ['./tsconfig.json'],
       }),
+      tailwindcss(),
     ],
   },
 })
